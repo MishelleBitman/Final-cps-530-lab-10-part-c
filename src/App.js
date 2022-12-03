20a840c82159a0bf50bc669449d57ec0ce4b2510
MishelleBitman <70034068+MishelleBitman@users.noreply.github.com>
2022-12-03 17:21:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div class="header">CPS 530 Lab 10 Part C</div>
+      <div class="header">By Mishelle Bitman</div>
+      <br></br>
+      
+      <div class="text">The framework I chose for this lab is React. I encountered many issues while doing this part of the lab because I had no prior knowledge or any clue on how to start. I read many different articles and each one had a different complicated way of how to start! But, essentially I understood that I need to build an environment and did so by downloading Node.js on my terminal. I thought the hard part was behind me, but then I used "Code ." to code this program with VSCode. It had so many different files and folders and I was very overwhelmed and confused about how to start and where. Then, I spent 3 hours trying to set a background colour for this web just to realize there is a body class in the file "index.css". But after understanding what each file is doing and getting rid of unnecessary files, I managed to style the web with no problems and found it simple.               </div>
+      <img class="pic" src="https://www.patterns.dev/img/reactjs/react-logo@3x.svg"></img>
+      </div>
   );
 }
 
